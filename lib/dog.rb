b848ae65d6d7fbc14d82dc1f7a8cd6f751e0c64c
Ashley Norton <nortonashleym@gmail.com>
2019-09-09 01:12:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 # Your code goes here!
 class Dog 
-  def .name=(dogs_name)
+  def name=(dogs_name)
     @this_dogs_name = dogs_name
   end 
   
-  def .name 
+  def name 
     @this_dogs_name
   end 
   
-  def .bark
+  def bark
     puts "woof!"
   end 
 end 
